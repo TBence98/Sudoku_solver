640799260a8996f49a863d437ce98580f02aa0eb
Tbence98 <tlrbence@gmail.com>
2023-02-05 18:52:14 +0100

    make SudokuSolver.jsx component

---
+++ src/hooks/useSolveSudoku.jsx
@@ -0,0 +1,180 @@
+const useSolveSudoku = () => {
+    function solve(board) {
+        // THIS FUNCTION WORKS.
+        // Board -> Board
+        // solves the given sudoku board
+        // ASSUME the given sudoku board is valid
+        if (solved(board)) {
+            return board;
+        } else {
+            const possibilities = nextBoards(board);
+            const validBoards = keepOnlyValid(possibilities);
+            return searchForSolution(validBoards);
+        }
+    }
+
+    // ______TESTS______ //
+    // console.log(solve(bd4))
+    // ______TESTS______ //
+
+    function searchForSolution(boards) {
+        // List[Board] -> Board or false
+        // finds a valid solution to the sudoku problem
+        if (boards.length < 1) {
+            return false;
+        } else {
+            // backtracking search for solution
+            const first = boards.shift();
+            const tryPath = solve(first);
+            if (tryPath !== false) {
+                return tryPath;
+            } else {
+                return searchForSolution(boards);
+            }
+        }
+    }
+
+    function solved(board) {
+        // THIS FUNCTION WORKS.
+        // Board -> Boolean
+        // checks to see if the given puzzle is solved
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                if (board[i][j] === "") {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // ______TESTS______ //
+    // console.log(solved(bd3))
+    // ______TESTS______ //
+
+    function nextBoards(board) {
+        // THIS FUNCTION WORKS.
+        // Board -> List[Board]
+        // finds the first emply square and generates 9 different boards filling in that square with numbers 1...9
+        const res = [];
+        const firstEmpty = findEmptySquare(board);
+        if (firstEmpty !== undefined) {
+            const y = firstEmpty[0];
+            const x = firstEmpty[1];
+            for (let i = 1; i <= 9; i++) {
+                const newBoard = [...board];
+                const row = [...newBoard[y]];
+                row[x] = i.toString();
+                newBoard[y] = row;
+                res.push(newBoard);
+            }
+        }
+        return res;
+    }
+
+    function findEmptySquare(board) {
+        // THIS FUNCTION WORKS.
+        // Board -> [Int, Int]
+        // (get the i j coordinates for the first empty square)
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                if (board[i][j] === "") {
+                    return [i, j];
+                }
+            }
+        }
+    }
+
+    // ______TESTS______ //
+    // console.log(nextBoards(bd3))
+    // console.log(findEmptySquare(bd3))
+    // ______TESTS______ //
+
+    function keepOnlyValid(boards) {
+        return boards.filter((b) => validBoard(b));
+    }
+
+    // ______TESTS______ //
+    // console.log(keepOnlyValid([bd1, bd2, bd3]))
+    // ______TESTS______ //
+
+    function validBoard(board) {
+        // THIS FUNCTION WORKS.
+        // Board -> Boolean
+        // checks to see if given board is valid
+        return rowsGood(board) && columnsGood(board) && boxesGood(board);
+    }
+
+    function rowsGood(board) {
+        // THIS FUNCTION WORKS.
+        // Board -> Boolean
+        // makes sure there are no repeating numbers for each row
+        for (let i = 0; i < 9; i++) {
+            const cur = [];
+            for (let j = 0; j < 9; j++) {
+                if (cur.includes(board[i][j])) {
+                    return false;
+                } else if (board[i][j] !== "") {
+                    cur.push(board[i][j]);
+                }
+            }
+        }
+        return true;
+    }
+
+    function columnsGood(board) {
+        // THIS FUNCTION WORKS.
+        // Board -> Boolean
+        // makes sure there are no repeating numbers for each column
+        for (let i = 0; i < 9; i++) {
+            const cur = [];
+            for (let j = 0; j < 9; j++) {
+                if (cur.includes(board[j][i])) {
+                    return false;
+                } else if (board[j][i] !== "") {
+                    cur.push(board[j][i]);
+                }
+            }
+        }
+        return true;
+    }
+
+    function boxesGood(board) {
+        // transform this everywhere to update res
+        const boxCoordinates = [
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [1, 0],
+            [1, 1],
+            [1, 2],
+            [2, 0],
+            [2, 1],
+            [2, 2],
+        ];
+        // THIS FUNCTION WORKS.
+        // Board -> Boolean
+        // makes sure there are no repeating numbers for each box
+        for (let y = 0; y < 9; y += 3) {
+            for (let x = 0; x < 9; x += 3) {
+                // each traversal should examine each box
+                const cur = [];
+                for (let i = 0; i < 9; i++) {
+                    const coordinates = [...boxCoordinates[i]];
+                    coordinates[0] += y;
+                    coordinates[1] += x;
+                    if (cur.includes(board[coordinates[0]][coordinates[1]])) {
+                        return false;
+                    } else if (board[coordinates[0]][coordinates[1]] !== "") {
+                        cur.push(board[coordinates[0]][coordinates[1]]);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    return solve;
+};
+
+export default useSolveSudoku;
--- src/pages/SudokuSolver.jsx
+++ src/pages/SudokuSolver.jsx
@@ -1,5 +1,162 @@
+import { useState, useEffect } from "react";
+import useSolveSudoku from "../hooks/useSolveSudoku";
+
+import classes from "./SudokuSolver.module.css";
+
+let inputsChangedByUser = [];
+
 const SudokuSolver = () => {
-    return <h1>SudokuSolver</h1>;
+    const [tableElements, setTableElements] = useState([]);
+    const [tableValues, setTableValues] = useState([]);
+    const [isInitialRender, setIsInitialRender] = useState(true);
+    const solve = useSolveSudoku();
+
+    useEffect(() => {
+        if (localStorage.getItem("inputsChangedByUser")) {
+            inputsChangedByUser = JSON.parse(
+                localStorage.getItem("inputsChangedByUser")
+            );
+            createDefaultValues(inputsChangedByUser);
+        } else {
+            createDefaultValues();
+        }
+    }, []);
+
+    function createDefaultValues(inputsByUser = []) {
+        const blankCell = "";
+        const defaultValues = [];
+        for (let i = 0; i <= 8; i++) {
+            const row = [];
+            for (let j = 0; j <= 8; j++) {
+                const changedByUsercell = inputsByUser.find(
+                    (input) =>
+                        input.x === j.toString() && input.y === i.toString()
+                );
+                row.push(
+                    changedByUsercell ? changedByUsercell.value : blankCell
+                );
+            }
+            defaultValues.push(row);
+        }
+        setTableValues(defaultValues);
+    }
+
+    useEffect(() => {
+        if (tableValues.length > 0) {
+            createTableElements();
+            setIsInitialRender(false);
+        }
+    }, [tableValues]);
+
+    function createTableElements() {
+        let shiftX = 0;
+        let shiftY = 0;
+        const tableElements = [];
+        for (let i = 1; i < 4; i++) {
+            for (let j = 1; j < 4; j++) {
+                const subgrid = [];
+                for (let y = shiftY; y < 3 + shiftY; y++) {
+                    const subgridRow = [];
+                    for (let x = shiftX; x < 3 + shiftX; x++) {
+                        const isHighlighted =
+                            isValueSetByUser(x, y) && !isInitialRender;
+
+                        subgridRow.push(
+                            <input
+                                type="number"
+                                className={`${classes.cell} ${
+                                    isHighlighted ? classes.cell__active : ""
+                                }`}
+                                onChange={cellChangeHandler}
+                                data-x={x}
+                                data-y={y}
+                                defaultValue={tableValues[y][x]}
+                            />
+                        );
+                    }
+                    subgrid.push(...subgridRow);
+                }
+                shiftX += 3;
+                tableElements.push(
+                    <div className={classes.grid} key={Math.random()}>
+                        {...subgrid}
+                    </div>
+                );
+            }
+            shiftX = 0;
+            shiftY += 3;
+        }
+
+        function isValueSetByUser(x, y) {
+            return inputsChangedByUser.some(
+                (input) => input.x === x.toString() && input.y === y.toString()
+            );
+        }
+
+        setTableElements(tableElements);
+    }
+
+    function cellChangeHandler(event) {
+        const y = event.target.dataset.y;
+        const x = event.target.dataset.x;
+        const value = event.target.value;
+
+        const isChangedValue = inputsChangedByUser.some(
+            (input) => input.x === x && input.y === y
+        );
+
+        if (value === "" && isChangedValue) {
+            //remove from the array to prevent memory leak
+            console.log("remove from array");
+            inputsChangedByUser = inputsChangedByUser.filter(
+                (input) => input.x !== x || input.y !== y
+            );
+        } else if (value !== "") {
+            // add to the array
+            inputsChangedByUser.push({ x, y, value });
+        }
+    }
+
+    function submitHandler(event) {
+        event.preventDefault();
+        const toBeSolvedTable = Array.from({ length: 9 }, () => []);
+        const formElements = event.target.elements;
+
+        for (let i = 0; i < formElements.length; i++) {
+            const input = formElements[i];
+
+            if (input.type === "number") {
+                toBeSolvedTable[input.dataset.y][input.dataset.x] = input.value;
+            }
+        }
+
+        localStorage.setItem(
+            "inputsChangedByUser",
+            JSON.stringify(inputsChangedByUser)
+        );
+
+        setTableValues(solve(toBeSolvedTable));
+    }
+
+    function resetHandler() {
+        inputsChangedByUser = [];
+        createDefaultValues();
+    }
+
+    return (
+        <>
+            <h1>SudokuSolver</h1>
+            <form onSubmit={submitHandler}>
+                <div className={`${classes.grid} ${classes.sudoku_container}`}>
+                    {tableElements}
+                </div>
+                <button type="submit">Solve</button>
+            </form>
+            <button type="button" onClick={resetHandler}>
+                Reset
+            </button>
+        </>
+    );
 };
 
 export default SudokuSolver;
