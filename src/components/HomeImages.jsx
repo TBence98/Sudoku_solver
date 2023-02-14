da633caf54d210967707908d221b8b6416102cad
Tbence98 <tlrbence@gmail.com>
2023-02-14 02:36:35 +0100

    add basic version of image carousel loop functionality

--- src/components/HomeImages.jsx
+++ src/components/HomeImages.jsx
@@ -1,51 +1,109 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import classes from "./HomeImages.module.css";
 
 const HomeImages = ({ images }) => {
-    const [translateX, setTranslateX] = useState(0);
-    const prevBtnIsDisabled = false;
-    const nextBtnIsDisabled = false;
+    const [renderedImages, setRenderedImages] = useState([]);
+    const [cachedImages, setCachedImages] = useState([]);
+
+    useEffect(() => {
+        const renderedImages = [];
+        const cachedImages = [];
+        for (let i = 0; i < images.length; i++) {
+            if (i < 5) {
+                renderedImages.push({ ...images[i], left: 25 * i });
+            } else if (i === images.length - 1) {
+                renderedImages.unshift({ ...images[i], left: -25 });
+            } else {
+                cachedImages.push({ ...images[i], left: null });
+            }
+        }
+
+        setRenderedImages(renderedImages);
+        setCachedImages(cachedImages);
+    }, []);
+
+    useEffect(() => {
+        const slideImages = setInterval(handleNext, 4000);
+
+        return () => clearInterval(slideImages);
+    }, [renderedImages]);
 
     function handlePrevious() {
-        setTranslateX((prevState) => prevState + 25);
+        const newRenderedImages = renderedImages.map((renderedImage) => {
+            return { ...renderedImage, left: renderedImage.left + 25 };
+        });
+        const newCachedImages = [...cachedImages];
+
+        newRenderedImages.unshift(newCachedImages.pop());
+        newCachedImages.unshift(newRenderedImages.pop());
+
+        newRenderedImages[0].left = -25;
+
+        setRenderedImages(newRenderedImages);
+        setCachedImages(newCachedImages);
     }
 
     function handleNext() {
-        setTranslateX((prevState) => prevState - 25);
+        const newRenderedImages = renderedImages.map((renderedImage) => {
+            return { ...renderedImage, left: renderedImage.left - 25 };
+        });
+        const newCachedImages = [...cachedImages];
+
+        newRenderedImages.push(newCachedImages.shift());
+        newCachedImages.push(newRenderedImages.shift());
+
+        newRenderedImages.at(-1).left = newRenderedImages.at(-2).left + 25;
+
+        setRenderedImages(newRenderedImages);
+        setCachedImages(newCachedImages);
     }
 
     return (
         <div className={classes.image_carousel}>
             <div className={classes.visible_images}>
-                <div
-                    className={classes.images}
-                    style={{
-                        transform: `translateX(${translateX}%)`,
-                        transition: "all 0.5s",
-                    }}
-                >
-                    {images.map((image, index) => (
-                        <img
-                            src={image.src}
-                            className={classes.image}
-                            key={index}
-                            alt={image.alt}
-                        />
-                    ))}
+                <div className={classes.images}>
+                    {images.map((image, index) => {
+                        const visibleImage = renderedImages.find(
+                            (renderedImage) => renderedImage.src === image.src
+                        );
+                        if (visibleImage) {
+                            return (
+                                <img
+                                    src={image.src}
+                                    className={classes.image}
+                                    key={index}
+                                    alt={image.alt}
+                                    style={{
+                                        left: `${visibleImage.left}%`,
+                                        transition: "all 0.7s",
+                                    }}
+                                />
+                            );
+                        }
+                        return (
+                            <img
+                                src={image.src}
+                                className={classes.image}
+                                key={index}
+                                alt={image.alt}
+                                style={{
+                                    top: "100%",
+                                }}
+                            />
+                        );
+                    })}
                 </div>
             </div>
             <button
                 className={`${classes.arrow} ${classes["arrow--left"]}`}
                 onClick={handlePrevious}
-                disabled={prevBtnIsDisabled}
             >
                 &#60;
             </button>
             <button
                 className={`${classes.arrow} ${classes["arrow--right"]}`}
                 onClick={handleNext}
-                disabled={nextBtnIsDisabled}
             >
                 &#62;
             </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,11 +13,38 @@ const Home = () => {
             <LineSeparator />
             <HomeImages
                 images={[
-                    { src: sudoku_img, alt: "sudoku" },
-                    { src: sudoku_img, alt: "sudoku" },
-                    { src: sudoku_img, alt: "sudoku" },
-                    { src: sudoku_img, alt: "sudoku" },
-                    { src: sudoku_img, alt: "sudoku" },
+                    {
+                        src: "http://via.placeholder.com/200?text=1",
+                        alt: "sudoku",
+                    },
+                    {
+                        src: "http://via.placeholder.com/200?text=2",
+                        alt: "sudoku",
+                    },
+                    {
+                        src: "http://via.placeholder.com/200?text=3",
+                        alt: "sudoku",
+                    },
+                    {
+                        src: "http://via.placeholder.com/200?text=4",
+                        alt: "sudoku",
+                    },
+                    {
+                        src: "http://via.placeholder.com/200?text=5",
+                        alt: "sudoku",
+                    },
+                    {
+                        src: "http://via.placeholder.com/200?text=6",
+                        alt: "sudoku",
+                    },
+                    {
+                        src: "http://via.placeholder.com/200?text=7",
+                        alt: "sudoku",
+                    },
+                    {
+                        src: "http://via.placeholder.com/200?text=8",
+                        alt: "sudoku",
+                    },
                 ]}
             />
             <LineSeparator />
